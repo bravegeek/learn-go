7e4d2f32374245bb8f37143f049653cfb9ab8a73
Greg Read <gregread@gmail.com>
2024-03-16 21:18:24 -0400

    get input

--- investment_calculator/investment_calculator.go
+++ investment_calculator/investment_calculator.go
@@ -7,7 +7,18 @@ import (
 
 func main() {
 	const inflationRate = 6.5
-	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
+	var investmentAmount float64
+	var years, expectedReturnRate float64
+	// years, expectedReturnRate := 10.0, 5.5
+
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
 
 	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
