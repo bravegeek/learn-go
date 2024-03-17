cc5753ca159690a0ca29f04d9783a22e487ba782
Greg Read <gregread@gmail.com>
2024-03-16 21:45:00 -0400

    profit calc

---
+++ profit_calc/profit_calculator.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// var revenue float64
+	var revenue, expenses, taxRate float64
+	// var earnings_before_tax, earnings_after_tax, ratio float64
+
+	fmt.Print("Revenue: ")
+	fmt.Scan(&revenue)
+
+	fmt.Print("Expenses: ")
+	fmt.Scan(&expenses)
+
+	fmt.Print("Tax Rate: ")
+	fmt.Scan(&taxRate)
+
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+	ratio := ebt / profit
+
+	fmt.Print("EBT: ")
+	fmt.Println(ebt)
+	fmt.Print("Profit: ")
+	fmt.Println(profit)
+	fmt.Print("Ratio: ")
+	fmt.Println(ratio)
+}
