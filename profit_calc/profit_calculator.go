33e0c65291d21ff4266618a1d99d9e6d4cf1354f
Greg Read <gregread@gmail.com>
2024-03-17 21:36:59 -0400

    format output

--- profit_calc/profit_calculator.go
+++ profit_calc/profit_calculator.go
@@ -5,36 +5,26 @@ import (
 )
 
 func main() {
-	// var revenue float64
-	var revenue, expenses, taxRate float64
+	// var revenue, expenses, taxRate float64
 	// var earnings_before_tax, earnings_after_tax, ratio float64
 
 	// fmt.Print("Revenue: ")
 	// fmt.Scan(&revenue)
-	revenue = getUserInput("Revenue")
+	revenue := getUserInput("Revenue")
+	expenses := getUserInput("Expenses")
+	taxRate := getUserInput("Tax Rate")
 
-	expenses = getUserInput("Expenses")
+	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
 
-	taxRate = getUserInput("Tax Rate")
-
-	// ebt := revenue - expenses
-	// profit := ebt * (1 - taxRate/100)
-	// ratio := ebt / profit
-	ebt, profit, ratio := calculateProfit(revenue, expenses, taxRate)
-
-	// fmt.Print("EBT: ")
-	fmt.Println("EBT: ", ebt)
-	// fmt.Print("Profit: ")
-	fmt.Println("Profit: ", profit)
-	// fmt.Print("Ratio: ")
-	fmt.Println("Ratio: ", ratio)
+	fmt.Printf("EBT: %.1f\n", ebt)
+	fmt.Printf("Profit: %.1f\n", profit)
+	fmt.Printf("Ratio: %.3f\n", ratio)
 }
 
-func calculateProfit(revenue, expenses, taxRate float64) (ebt, profit, ratio float64) {
+func calculateFinancials(revenue, expenses, taxRate float64) (ebt, profit, ratio float64) {
 	ebt = revenue - expenses
 	profit = ebt * (1 - taxRate/100)
 	ratio = ebt / profit
-
 	return ebt, profit, ratio
 }
 
