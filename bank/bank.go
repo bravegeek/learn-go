9ad6b5b8dcc77cf494b011b2d4cbef2ca9fffe04
Greg Read <gregread@gmail.com>
2024-03-24 14:53:08 -0400

    bank loop

--- bank/bank.go
+++ bank/bank.go
@@ -4,41 +4,47 @@ import "fmt"
 
 func main() {
 	accountBalance := 1000.00
-	menuChoice := getMenuChoice()
 
-	if menuChoice == 1 {
-		fmt.Println("Your balance is", accountBalance)
-	} else if menuChoice == 2 {
-		depositAmt := getUserInput("Deposit amount: ")
+	fmt.Println("Welcome to Go Bank!")
 
-		if depositAmt <= 0 {
-			fmt.Println("Invalid amount. Must be greater than 0.")
-			return
+	for {
+		menuChoice := getMenuChoice()
+
+		if menuChoice == 1 {
+			fmt.Println("Your balance is", accountBalance)
+		} else if menuChoice == 2 {
+			depositAmt := getUserInput("Deposit amount: ")
+
+			if depositAmt <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0.")
+				return
+			}
+
+			accountBalance += depositAmt
+			fmt.Println("Your balance is now", accountBalance)
+		} else if menuChoice == 3 {
+			withdrawalAmt := getUserInput("Withdrawal amount: ")
+
+			if withdrawalAmt <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0.")
+				return
+			}
+			if withdrawalAmt > accountBalance {
+				fmt.Println("Invalid amount. You can't withdraw more than you have.")
+				return
+			}
+
+			accountBalance -= withdrawalAmt
+			fmt.Println("Your balance is now", accountBalance)
+		} else {
+			println("Goodbye!")
+			break
 		}
-
-		accountBalance += depositAmt
-		fmt.Println("Your balance is now", accountBalance)
-	} else if menuChoice == 3 {
-		withdrawalAmt := getUserInput("Withdrawal amount: ")
-
-		if withdrawalAmt <= 0 {
-			fmt.Println("Invalid amount. Must be greater than 0.")
-			return
-		}
-		if withdrawalAmt > accountBalance {
-			fmt.Println("Invalid amount. You can't withdraw more than you have.")
-			return
-		}
-
-		accountBalance -= withdrawalAmt
-		fmt.Println("Your balance is now", accountBalance)
-	} else {
-		println("Goodbye!")
 	}
+	fmt.Println("Thank you for choosing Go bank!")
 }
 
 func getMenuChoice() (menuChoice int) {
-	fmt.Println("Welcome to Go Bank!")
 	fmt.Println("What do you want to do?")
 	fmt.Println("1. Check balance")
 	fmt.Println("2. Deposit money")
@@ -53,4 +59,8 @@ func getUserInput(message string) (userInput float64) {
 	fmt.Printf("%v", message)
 	fmt.Scan(&userInput)
 	return userInput
+}
+
+func processUserInput(menuChoice int) {
+
 }
