61a826cfb974252d250b0f32bef4ada732695c26
Greg Read <gregread@gmail.com>
2024-03-25 20:14:11 -0400

    profit excercise 2

--- profit_calc/profit_calculator.go
+++ profit_calc/profit_calculator.go
@@ -1,20 +1,19 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 )
 
 func main() {
-	// var revenue, expenses, taxRate float64
-	// var earnings_before_tax, earnings_after_tax, ratio float64
-
-	// fmt.Print("Revenue: ")
-	// fmt.Scan(&revenue)
 	revenue := getUserInput("Revenue")
 	expenses := getUserInput("Expenses")
 	taxRate := getUserInput("Tax Rate")
 
 	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
+
+	writeFinancialsToFile(ebt, profit, ratio)
 
 	fmt.Printf("EBT: %.1f\n", ebt)
 	fmt.Printf("Profit: %.1f\n", profit)
@@ -31,5 +30,28 @@ func calculateFinancials(revenue, expenses, taxRate float64) (ebt, profit, ratio
 func getUserInput(message string) (userInput float64) {
 	fmt.Printf("%v: ", message)
 	fmt.Scan(&userInput)
+
+	userInput, err := validateUserInput(userInput)
+
+	if err != nil {
+		fmt.Println(err)
+		panic("aaaaah!")
+	}
+
 	return userInput
+}
+
+func validateUserInput(userInput float64) (float64, error) {
+	if userInput < 0 || userInput == 0 {
+		return userInput, errors.New("value must be greater than 0")
+	}
+	return userInput, nil
+}
+
+const financialsFile = "financials.txt"
+
+func writeFinancialsToFile(ebt float64, profit float64, ratio float64) {
+
+	stringToWrite := []byte(fmt.Sprintf("EBT: %.1f\nProfit: %.1f\nRatio: %.3f\n", ebt, profit, ratio))
+	os.WriteFile(financialsFile, stringToWrite, 0644)
 }
