b8a2278041bd73f31a0540b7be356894727415b9
Greg Read <gregread@gmail.com>
2024-04-07 17:52:59 -0400

    struct in a package

--- structs/structs.go
+++ structs/structs.go
@@ -1,64 +1,28 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"time"
+
+	"example.com/structs/user"
 )
-
-type user struct {
-	firstName string
-	lastName  string
-	birthDate string
-	created   time.Time
-}
-
-func (u user) outputUserDetails() {
-	fmt.Println(u.firstName, u.lastName, u.birthDate)
-}
-
-func (u *user) clearUserName() {
-	u.firstName = ""
-	u.lastName = ""
-}
-
-func newUser(firstName, lastName, birthDate string) (*user, error) {
-	// func newUser(firstName, lastName, birthDate string) user {
-	if firstName == "" || lastName == "" || birthDate == "" {
-		return nil, errors.New("Missing values")
-	}
-
-	return &user{
-		firstName: firstName,
-		lastName:  lastName,
-		birthDate: birthDate,
-		created:   time.Now(),
-	}, nil
-}
 
 func main() {
 	userFirstName := getUserData("Enter your first name: ")
 	userLastName := getUserData("Enter your last name: ")
 	userBirthDate := getUserData("Enter your birth date: ")
 
-	var appUser *user
-	appUser, err := newUser(userFirstName, userLastName, userBirthDate)
+	var appUser *user.User
+
+	appUser, err := user.New(userFirstName, userLastName, userBirthDate)
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	// appUser = user{
-	// 	firstName: userFirstName,
-	// 	lastName:  userLastName,
-	// 	birthDate: userBirthDate,
-	// 	created:   time.Now(),
-	// }
-
-	appUser.outputUserDetails()
-	appUser.clearUserName()
-	appUser.outputUserDetails()
+	appUser.OutputUserDetails()
+	appUser.ClearUserName()
+	appUser.OutputUserDetails()
 }
 
 func getUserData(promptText string) string {
@@ -67,7 +31,3 @@ func getUserData(promptText string) string {
 	fmt.Scanln(&value)
 	return value
 }
-
-// func outputUserDetails(u *user) {
-// 	fmt.Println(u.firstName, u.lastName, u.birthDate)
-// }
