4c1a68ccf36f003fbf09f506abc97d117ce98f97
Greg Read <gregread@gmail.com>
2024-05-05 18:49:44 -0400

    slices

--- lists/lists.go
+++ lists/lists.go
@@ -9,9 +9,11 @@ import "fmt"
 // }
 
 func main() {
+
 	productNames := [4]string{"A book"}
 
 	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
+
 	fmt.Println(prices)
 
 	productNames[2] = "A Carpet"
@@ -19,4 +21,7 @@ func main() {
 	fmt.Println(productNames)
 
 	fmt.Println(prices[0])
+
+	featuredPrices := prices[1:3] //1st number is inclusive, 2nd is exclusive
+	fmt.Println(featuredPrices)
 }
