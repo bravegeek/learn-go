bbbd5541971ac3e4cfcfe56a98ec9e9a54be1ae7
Greg Read <gregread@gmail.com>
2024-05-05 21:22:43 -0400

    maps

---
+++ lists/maps.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// websites:=[]string{"https://google.com","https://aws.com"}
+	websites := map[string]string{ //like a dictionary
+		"Google":              "https://google.com",
+		"Amazon Web Services": "https://aws.com",
+	}
+	fmt.Println(websites)
+	fmt.Println(websites["Amazon Web Services"])
+
+	websites["LinkedIn"] = "https://linkedin.com"
+	fmt.Println(websites)
+
+	delete(websites, "Google")
+	fmt.Println(websites)
+}
